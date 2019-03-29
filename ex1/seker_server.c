07243d8e6f4c487c63e1f1dbceaf7ed65d4c421b
barzamir1 <bar820@gmail.com>
2019-03-29 17:28:13 +0300

    add list_of_courses

--- ex1/seker_server.c
+++ ex1/seker_server.c
@@ -9,9 +9,9 @@
 #include "seker_helpers.h"
 
 #define min(a,b) a<b ? a : b
-#define IO_ERROR -5
-#define TCP_RECEIVE_ERROR 5
-#define TCP_SEND_ERROR 7
+#define IO_ERROR 2
+#define TCP_RECEIVE_ERROR 3
+#define TCP_SEND_ERROR 4
 
 struct user_details{
     char userName[MAXIMUM_USERNAME_LENGTH];
@@ -26,7 +26,7 @@ char *course_list_path;
 int listen_port = DEFAULT_PORT;
 struct sockaddr_in server_addr;
 
-/*reads users_file and stores all usernames and passwords in Users*/
+/*read users_file and stores all usernames and passwords in Users*/
 void InitUsers(char *users_file){
     FILE *fp = fopen(users_file, "r");
     if(fp == NULL){
@@ -79,7 +79,7 @@ void InitServerFolder(){
 	fclose(courselist);
 }
 
-/*receives username and password from the client, validates and returns the username*/
+/*receive username and password from the client, validate and return the username*/
 char * GetAndValidateUsername(int client_fd){
 	char *username = calloc(sizeof(char), MAXIMUM_USERNAME_LENGTH+1);
 	char password[MAXIMUM_USERNAME_LENGTH+1]={0};
@@ -109,10 +109,40 @@ char * GetAndValidateUsername(int client_fd){
 	return NULL;
 }
 
-int courseNumExist(int courseNum){
+/*send entire file to client, line by line*/
+int sendFile(int client_fd, FILE * file, int bufferLen, char *filename){
+	size_t length = bufferLen;
+	char *buffer = calloc(sizeof(char), length);
+	if(!buffer){
+		printf("ERROR: can't allocate buffer for %s\n", filename);
+		fclose(file);
+		return IO_ERROR;
+	}
+	int bytesread;
+	while(1){
+		if((bytesread = getline(&buffer, &length, file)) < 0){
+			break;
+		}
+		if(sendString(client_fd, buffer)){
+			printf("ERROR: failed to send string to client %d\n", client_fd);
+			free(buffer);
+			fclose(file);
+			return TCP_SEND_ERROR;
+		}
+	}
+	sendString(client_fd, END_OF_LIST);
+	free(buffer);
+	fclose(file);
+	return SUCCESS;
+}
+
+/*return SUCCESS if courseNum exists,
+ *-1 if courseNum doesn't exist
+ *or an error number*/
+int findCourseNum(int courseNum){
 	FILE *courseList = fopen(course_list_path, "a+");
 	if(courseList == NULL){
-		printf("ERROR: can't open %s", course_list_path);
+		printf("ERROR: can't open %s\n", course_list_path);
 		return IO_ERROR;
 	}
 	char buffer[MAXIMUM_COURSE_NAME_LENGTH+10]={0};
@@ -120,45 +150,57 @@ int courseNumExist(int courseNum){
 	size_t length;
 	char *bufferAddr = (char*)&buffer;
 	while(getline(&bufferAddr, &length, courseList) > 0){
-		currCourseNum = strtok(bufferAddr, "\t");
+		currCourseNum = strtok(bufferAddr, ":");
 		if(atoi(currCourseNum) == courseNum){
 			fclose(courseList);
-			return 1;
+			return SUCCESS;
 		}
 	}
 	//course doesn't exist
 	fclose(courseList);
-	return 0;
+	return -1;
 }
 
-/*receives courseNum from the client. sends ERROR if courseNum exists
- * if not, receives course name from the client and adds to courseList
+int listAllCourses(int client_fd){
+	FILE *courselist = fopen(course_list_path, "r");
+	if(courselist == NULL){
+		printf("ERROR: can't open %s\n", course_list_path);
+		return IO_ERROR;
+	}
+	//send entire courselist file to client, line by line
+	int result = sendFile(client_fd, courselist, MAXIMUM_COURSE_NAME_LENGTH +10, "courselist");
+	return result;
+}
+
+
+/*receive courseNum from the client. send ERROR if courseNum exists
+ * if not, receive course name from the client and add to courseList
  */
 int addCousre(int client_fd){
 	int courseNum = receivePositiveInt(client_fd);
 
-	int result = courseNumExist(courseNum);
-	if(result<0){
-		//error occurred
-		return result;
-	}
-	if(result>0){
+	int result = findCourseNum(courseNum);
+	if(result==SUCCESS){
 		//courseNum already exist
 		printf("course num %d already exist", courseNum);
 		sendPositiveInt(client_fd, ERROR);
 		return SUCCESS;
 	}
+	if(result > 0){
+		//error occurred
+		return result;
+	}
 
 	//course number doesn't exist in the list
 	FILE *courseList = fopen(course_list_path, "a+");
 	if(courseList==NULL){
-		printf("ERROR: can't open %s", course_list_path);
+		printf("ERROR: can't open %s\n", course_list_path);
 		return IO_ERROR;
 	}
 	sendPositiveInt(client_fd, SUCCESS);
 	char buffer[MAXIMUM_COURSE_NAME_LENGTH+10]={0};
 	receiveString(client_fd, (char*)&buffer);
-	fprintf(courseList, "%d\t%s\n", courseNum, buffer);
+	fprintf(courseList, "%d:\t%s\n", courseNum, buffer);
 	fclose(courseList);
 
 	return SUCCESS;
@@ -167,17 +209,17 @@ int addCousre(int client_fd){
 int rateCourse(int client_fd, char *username){
 	int courseNum = receivePositiveInt(client_fd);
 	if(courseNum==-ERROR){
-		printf("ERROR: can't receive courseNum from client %d", client_fd);
+		printf("ERROR: can't receive courseNum from client %d\n", client_fd);
 		return TCP_RECEIVE_ERROR;
 	}
-	int result = courseNumExist(courseNum);
-	if( result < 0)
-		return result;
-	if(result==0){
+	int result = findCourseNum(courseNum);
+	if(result < 0){
 		//courseNum doesn't exist
 		sendPositiveInt(client_fd, ERROR);
 		return SUCCESS;
 	}
+	if( result > 0) //error occurred
+		return result;
 	else
 		sendPositiveInt(client_fd, SUCCESS);
 
@@ -186,18 +228,18 @@ int rateCourse(int client_fd, char *username){
 	sprintf((char*)&courseFilePath, "%s/%d", dir_path, courseNum);
 	FILE *courseFile = fopen(courseFilePath, "a+");
 	if(courseFile==NULL){
-		printf("ERROR: can't open %s", courseFilePath);
+		printf("ERROR: can't open %s\n", courseFilePath);
 		return IO_ERROR;
 	}
 	int ratingValue = receivePositiveInt(client_fd);
 	if(ratingValue==-ERROR){
-		printf("ERROR: can't receive ratingValue from client %d", client_fd);
+		printf("ERROR: can't receive ratingValue from client %d\n", client_fd);
 		fclose(courseFile);
 		return -ERROR;
 	}
 	char ratingText[MAXIMUM_RATING_TEXT_LENGTH+10]={0};
 	if(receiveString(client_fd, (char*)&ratingText)<0){
-		printf("ERROR: can't receive rating text from client %d", client_fd);
+		printf("ERROR: can't receive rating text from client %d\n", client_fd);
 		fclose(courseFile);
 		return -ERROR;
 	}
@@ -214,12 +256,12 @@ int getRate(int client_fd){
 		return TCP_RECEIVE_ERROR;
 	}
 	//check courseNum exists
-	int courseExist = courseNumExist(courseNum);
-	if(courseExist==0){
+	int courseExist = findCourseNum(courseNum);
+	if(courseExist < 0){
 		printf("ERROR: course %d doesn't exist\n", courseNum);
 		return SUCCESS;
 	}
-	if(courseExist < 0){
+	if(courseExist > 0){ //error occurred
 		return courseExist;
 	}
 	//open course file
@@ -228,47 +270,41 @@ int getRate(int client_fd){
 	sprintf((char*)&courseFilePath, "%s/%d", dir_path, courseNum);
 	FILE *courseFile = fopen((char*)&courseFilePath, "a+");
 	if(courseFile == NULL){
-		printf("ERROR: can't open course %d file", courseNum);
+		printf("ERROR: can't open course %d file\n", courseNum);
 		return IO_ERROR;
 	}
 	//send entire file to client, line by line
-	char *buffer; //[MAXIMUM_USERNAME_LENGTH + MAXIMUM_RATING_TEXT_LENGTH + 10];
-	size_t length=0;
-	int bytesread;
-	while(1){
-        if((bytesread = getline(&buffer, &length, courseFile)) < 0){
-            break;
-        }
-		if(sendString(client_fd, buffer)){
-			printf("ERROR: can't send rating to client\n");
-			free(buffer);
-			return TCP_SEND_ERROR;
-		}
-	}
-	sendString(client_fd, END_OF_LIST);
-	free(buffer);
-	return SUCCESS;
+	int result = sendFile(client_fd, courseFile, MAXIMUM_RATING_TEXT_LENGTH + 10, (char*)&courseFilePath);
+	return result;
 }
 
-/*receives commands from client and responses accordingly*/
+/*receive commands from client and response accordingly*/
 int HandleCommands(int client_fd, char *username){
 	int command;
 	int result;
 	while(1){
 		command = receivePositiveInt(client_fd);
 		switch(command){
+			case LIST_OF_COURSES:
+				if((result = listAllCourses(client_fd)))
+					return result;
+				break;
 			case ADD_COURSE:
-				if((result = addCousre(client_fd)) < 0)
+				if((result = addCousre(client_fd)))
 					return result;
 				break;
 			case RATE_COURSE:
-				if((result = rateCourse(client_fd, username)) < 0)
+				if((result = rateCourse(client_fd, username)))
 					return result;
 				break;
 			case GET_RATE:
-				if((result = getRate(client_fd)) < 0)
+				if((result = getRate(client_fd)))
 					return result;
 				break;
+			case QUIT:
+				close(client_fd);
+				free(username);
+				return SUCCESS;
 			default:
 				break;
 		}
@@ -280,16 +316,16 @@ int HandleCommands(int client_fd, char *username){
 void StartListening(){
     int socket_fd = socket(PF_INET, SOCK_STREAM, 0); //set socket to (IPv4, TCP, default protocol)
     if(socket_fd<0){
-        printf("ERROR: can't create socket");
+        printf("ERROR: can't create socket\n");
         return;
     }
     socklen_t adrrsize = sizeof(server_addr);
     if(bind(socket_fd, (struct sockaddr*)&server_addr, adrrsize)){
-        printf("ERROR: can't bind socket to server_addr");
+        printf("ERROR: can't bind socket to server_addr\n");
         return;
     }
     if(listen(socket_fd, 10)){
-        printf("ERROR: listen() failed");
+        printf("ERROR: listen() failed\n");
         return;
     }
 
@@ -299,15 +335,19 @@ void StartListening(){
     while(1){
         int client_fd = accept(socket_fd, (struct sockaddr*)&client_addr, &client_addrsize);
         if(client_fd<0){
-            printf("ERROR: can't accept new connection");
+            printf("ERROR: can't accept new connection\n");
             return;
         }
         char *username = GetAndValidateUsername(client_fd);
         if(username == NULL){
-        	printf("ERROR: username or password are incorrect");
+        	printf("ERROR: username or password are incorrect\n");
         	close(client_fd);
         }
-        HandleCommands(client_fd, username);
+        int result = HandleCommands(client_fd, username);
+        if(result){
+        	close(client_fd);
+        	free(username);
+        }
     }
 
     close(socket_fd);
